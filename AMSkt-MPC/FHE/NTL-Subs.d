9d73e33cb0037e321e3be0a4d540114a8822fcaf
tsharry2201 <141011128+tsharry2201@users.noreply.github.com>
2024-10-18 23:52:46 +0800

    Add files via upload

---
+++ AMSkt-MPC/FHE/NTL-Subs.d
@@ -0,0 +1,14 @@
+FHE/NTL-Subs.o: FHE/NTL-Subs.cpp FHE/NTL-Subs.h FHE/Ring.h \
+ Tools/octetStream.h Networking/data.h Exceptions/Exceptions.h \
+ Networking/sockets.h FHE/FFT_Data.h Math/modp.h Tools/random.h \
+ Tools/sha1.h Tools/aes.h Math/bigint.h Tools/int.h Tools/avx_memcpy.h \
+ Math/Zp_Data.h Math/gfp.h Math/gf2n.h Math/gf2nlong.h Math/field_types.h \
+ FHE/P2Data.h FHE/Matrix.h FHE/PPData.h FHE/FHE_Params.h \
+ FHE/DiscreteGauss.h FHE/Generator.h Math/Setup.h FHE/QGroup.h \
+ FHE/NoiseBounds.h Tools/mkpath.h FHEOffline/Proof.h FHE/Ciphertext.h \
+ FHE/FHE_Keys.h FHE/Rq_Element.h FHE/Ring_Element.h FHE/tools.h \
+ FHE/Random_Coins.h FHE/AddableVector.h FHE/Plaintext.h \
+ FHEOffline/config.h FHEOffline/DataSetup.h Networking/Player.h \
+ Networking/ServerSocket.h Tools/WaitQueue.h Tools/Signal.h \
+ Networking/Receiver.h Tools/time-func.h Networking/Sender.h \
+ FHEOffline/FullSetup.h
---
+++ AMSkt-MPC/FHE/Ring_Element.d
@@ -0,0 +1,6 @@
+FHE/Ring_Element.o: FHE/Ring_Element.cpp FHE/Ring_Element.h \
+ FHE/FFT_Data.h Math/modp.h Tools/octetStream.h Networking/data.h \
+ Exceptions/Exceptions.h Networking/sockets.h Tools/random.h Tools/sha1.h \
+ Tools/aes.h Math/bigint.h Tools/int.h Tools/avx_memcpy.h Math/Zp_Data.h \
+ Math/gfp.h Math/gf2n.h Math/gf2nlong.h Math/field_types.h FHE/Ring.h \
+ FHE/Generator.h FHE/FFT.h
