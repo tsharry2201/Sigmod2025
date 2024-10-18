302019e76ef4f086ead9603180bd6adea70c3696
tsharry2201 <141011128+tsharry2201@users.noreply.github.com>
2024-10-18 23:35:55 +0800

    Delete FHE directory

--- FHE/Ciphertext.d
+++
@@ -1,9 +0,0 @@
-FHE/Ciphertext.o: FHE/Ciphertext.cpp FHE/Ciphertext.h FHE/FHE_Keys.h \
- FHE/Rq_Element.h FHE/Ring_Element.h FHE/FFT_Data.h Math/modp.h \
- Tools/octetStream.h Networking/data.h Exceptions/Exceptions.h \
- Networking/sockets.h Tools/random.h Tools/sha1.h Tools/aes.h \
- Math/bigint.h Tools/int.h Tools/avx_memcpy.h Math/Zp_Data.h Math/gfp.h \
- Math/gf2n.h Math/gf2nlong.h Math/field_types.h FHE/Ring.h \
- FHE/Generator.h FHE/FHE_Params.h FHE/DiscreteGauss.h FHE/tools.h \
- FHE/Random_Coins.h FHE/AddableVector.h FHE/Plaintext.h FHE/P2Data.h \
- FHE/Matrix.h FHE/PPData.h FHE/Ciphertext.h
