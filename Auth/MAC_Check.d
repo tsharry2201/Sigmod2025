d6c49e5204ee83adb02691f49a3df7802558e6b2
tsharry2201 <141011128+tsharry2201@users.noreply.github.com>
2024-10-18 23:35:19 +0800

    Delete Auth directory

--- Auth/MAC_Check.d
+++
@@ -1,8 +0,0 @@
-Auth/MAC_Check.o: Auth/MAC_Check.cpp Auth/MAC_Check.h Math/Share.h \
- Math/gfp.h Math/gf2n.h Tools/random.h Tools/octetStream.h \
- Networking/data.h Exceptions/Exceptions.h Networking/sockets.h \
- Tools/sha1.h Tools/aes.h Math/gf2nlong.h Math/field_types.h \
- Math/bigint.h Tools/int.h Tools/avx_memcpy.h Math/modp.h Math/Zp_Data.h \
- Networking/Player.h Networking/ServerSocket.h Tools/WaitQueue.h \
- Tools/Signal.h Networking/Receiver.h Tools/time-func.h \
- Networking/Sender.h Auth/Summer.h Auth/Subroutines.h Tools/Commit.h
