3666b6c8ad99c8c52da478655ce5abb13ca83fc2
tsharry2201 <141011128+tsharry2201@users.noreply.github.com>
2024-10-18 23:35:30 +0800

    Delete Compiler directory

--- Compiler/tools.py
+++
@@ -1,9 +0,0 @@
-# (C) 2018 University of Bristol. See License.txt
-
-import itertools
-
-class chain(object):
-    def __init__(self, *args):
-        self.args = args
-    def __iter__(self):
-        return itertools.chain(*self.args)
