79b417f7f45b8f41c1cb8ccc350641697739afc7
tsharry2201 <141011128+tsharry2201@users.noreply.github.com>
2024-10-23 11:42:49 +0800

    Delete SIGMOD2025_cpp-main/Math directory

--- SIGMOD2025_cpp-main/Math/Setup.d
+++
@@ -1,5 +0,0 @@
-Math/Setup.o: Math/Setup.cpp Math/Setup.h Math/bigint.h \
- Exceptions/Exceptions.h Tools/int.h Tools/random.h Tools/octetStream.h \
- Networking/data.h Networking/sockets.h Tools/sha1.h Tools/aes.h \
- Tools/avx_memcpy.h Math/gfp.h Math/gf2n.h Math/gf2nlong.h \
- Math/field_types.h Math/modp.h Math/Zp_Data.h Tools/mkpath.h
--- SIGMOD2025_cpp-main/Math/bigint.d
+++
@@ -1,5 +0,0 @@
-Math/bigint.o: Math/bigint.cpp Math/bigint.h Exceptions/Exceptions.h \
- Tools/int.h Tools/random.h Tools/octetStream.h Networking/data.h \
- Networking/sockets.h Tools/sha1.h Tools/aes.h Tools/avx_memcpy.h \
- Math/gfp.h Math/gf2n.h Math/gf2nlong.h Math/field_types.h Math/bigint.h \
- Math/modp.h Math/Zp_Data.h
