d079789aca6e94cb402868bf1edeaea544e6d168
tsharry2201 <141011128+tsharry2201@users.noreply.github.com>
2024-10-23 11:50:40 +0800

    Add files via upload

---
+++ SIGMOD2025_cpp-main/AMSkt-MPC/Processor/Machine.d
@@ -0,0 +1,11 @@
+Processor/Machine.o: Processor/Machine.cpp Processor/Machine.h \
+ Processor/Memory.h Processor/Program.h Processor/Instruction.h \
+ Processor/Data_Files.h Math/gfp.h Math/gf2n.h Tools/random.h \
+ Tools/octetStream.h Networking/data.h Exceptions/Exceptions.h \
+ Networking/sockets.h Tools/sha1.h Tools/aes.h Math/gf2nlong.h \
+ Math/field_types.h Math/bigint.h Tools/int.h Tools/avx_memcpy.h \
+ Math/modp.h Math/Zp_Data.h Math/Share.h Processor/Buffer.h \
+ Tools/time-func.h Processor/InputTuple.h Tools/Lock.h \
+ Networking/Player.h Networking/ServerSocket.h Tools/WaitQueue.h \
+ Tools/Signal.h Networking/Receiver.h Networking/Sender.h Math/Integer.h \
+ Auth/MAC_Check.h Auth/Summer.h Processor/Online-Thread.h Math/Setup.h
